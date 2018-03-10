91250feb8ba35e86a1536554c4a68ccce517dbde
Kentarou Mamiya <kmamiya@logicalrabbit.jp>
2018-03-11 01:28:18 +0900

    [WIP]#50 added Highlight (this copied from ../src/ , and minor change)

---
+++ vuejs_ver/src/highlight.js
@@ -0,0 +1,125 @@
+const rangy = require("rangy");
+require("rangy/lib/rangy-classapplier.js");
+require("rangy/lib/rangy-highlighter.js");
+require("rangy/lib/rangy-serializer.js");
+
+class Highlight {
+  constructor(startOffset, endOffset, htmlClassName, base_node) {
+    this._startOffset = startOffset;
+    this._endOffset   = endOffset;
+    this.htmlClassName = htmlClassName;
+    this.base_node = base_node === undefined ? document.getElementById("viewer") : base_node;
+
+    this._createDom(this._startOffset, this._endOffset);
+  }
+
+  get className() {
+    return this.htmlClassName;
+  }
+
+  get BASE_NODE() {
+    return this.base_node;
+  }
+
+  get SCROLL_BASE_NODE_ID() {
+    return 'viewerWrapper';
+  }
+
+  get startOffset() {
+    return this.startOffset;
+  }
+
+  get endOffset() {
+    return this.endOffset;
+  }
+
+  get scrollOffset() {
+    return this._searchNodeScrollRoot(this.domElements[0]).offsetTop;
+  }
+
+  addClass(name){
+    this.domElements.forEach((e)=>{
+      $(e).addClass(name);
+    });
+  }
+
+  removeClass(name) {
+    this.domElements.forEach((e)=>{
+      $(e).removeClass(name);
+    });
+  }
+
+  _createDom(_startOffset, _endOffset) {
+    const selection = this._selectRange(_startOffset, _endOffset);
+    const temporaryElements = [];
+    const highlighter = rangy.createHighlighter();
+    highlighter.addClassApplier(rangy.createClassApplier(
+      this.className,
+      {
+        ignoreWhiteSpace: true,
+        onElementCreate: (element)=>{temporaryElements.push(element)},
+        useExistingElements: false
+      }
+    ));
+
+    highlighter.highlightSelection(
+      this.className,
+      {exclusive: false}
+    );
+    if (temporaryElements.length > 0){
+      this.domElements = temporaryElements;
+    }
+    selection.removeAllRanges();
+  }
+
+  _selectRange(startBodyOffset, endBodyOffset) {
+    if (startBodyOffset > endBodyOffset){
+      const tmp = startBodyOffset;
+      startBodyOffset = endBodyOffset;
+      endBodyOffset = tmp;
+    }
+
+    const start = this._nodeFromTextOffset(startBodyOffset);
+    const end = this._nodeFromTextOffset(endBodyOffset);
+    const selection = rangy.getSelection();
+    const range = rangy.createRange();
+    range.setStart(start.node, start.offset);
+    range.setEnd(end.node, end.offset);
+    selection.setSingleRange(range);
+
+    return selection;
+  }
+
+  _nodeFromTextOffset(offset, node){
+    if (undefined == node) {
+      node = this.BASE_NODE;
+    }
+    for (let i = 0; i < node.childNodes.length ; i++){
+      const child = node.childNodes[i];
+
+      if (child.nodeName == "#text"){
+        if (offset <= child.textContent.length){
+          return {offset:offset, node:child};
+        }
+        offset -= child.textContent.length;
+      } else{
+        const ret = this._nodeFromTextOffset(offset, child);
+        if (ret.node){
+          return ret;
+        }
+        offset = ret.offset;
+      }
+    }
+
+    return {offset:offset, node:null};
+  }
+
+  _searchNodeScrollRoot(element) {
+    while(element.offsetParent.id != this.SCROLL_BASE_NODE_ID && element.offsetParent != null) {
+      element = element.offsetParent;
+    }
+    return element;
+  }
+}
+
+module.exports = Highlight;
