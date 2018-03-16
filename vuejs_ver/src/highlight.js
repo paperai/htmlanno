f1075201166f3415cf79658c0eb9bc9ad06fa804
Kentarou Mamiya <kmamiya@logicalrabbit.jp>
2018-03-16 23:50:23 +0900

    [WIP]prototyping about highlight-tag append algorithm by original implement.

--- vuejs_ver/src/highlight.js
+++ vuejs_ver/src/highlight.js
@@ -49,27 +49,48 @@ class Highlight {
     });
   }
 
+  /**
+   * 1. _startOffset, _endOffsetが含まれるテキストノードを探す -> range
+   * 2. range.start.node === range.end.node の場合、ハイライトタグで括ったのち
+   *     2-1. parentNode.childNodes の中から range.start.node(end.nodeでもよい)を探しだす
+   *     2-2. 探し出したノードをstart.nodeの先頭からstart.offsetまでと入替
+   *     2-3. 探し出したノードの直後にハイライトタグノードを追加
+   *     2-4. ハイライトタグノードの直後にstart.nodeのend.offset以降を追加
+   * 3. range.start.node !== range.end.node の場合、
+   *     3-1. parentNode.childNodes の中から range.start.nodeを探し出す
+   *     3-2. 探し出したノードをstart.nodeの先頭からstart.offsetまでと入替
+   *     3-3. start.nodeのstart.offset以降からstart.node末尾までをハイライトタグで括る(ハイライトタグA)
+   *     3-4. ハイライトタグAを探し出したノードの直後に追加
+   *     3-5. end.nodeに達するまで、start.node以降のparentNode.childeNodes要素を処理
+   *         3-5-1. 隣接ノードのchildNodes(1-n) を処理
+   *             3-5-1-1, テキストノードまで下る
+   *             3-5-1-2. 下って見つけたテキストノード全体をハイライトタグで括る(ハイライトタグBn)
+   *             3-5-1-3. 下って見つけたテキストノードのparentNode.innerHTMLをハイライトタグBnと入替
+   *     3-6. end.nodeの先頭からend.offsetまでをハイライトタグで括る(ハイライトタグC)
+   *     3-7. parentNode.childNodesのend.nodeをハイライトタグCと入替
+   *     3-8. end.nodeのend.offset以降をハイライトタグCの直後に追加
+   */
   _createDom(_startOffset, _endOffset) {
-    const selection = this._selectRange(_startOffset, _endOffset);
-    const temporaryElements = [];
-    const highlighter = rangy.createHighlighter();
-    highlighter.addClassApplier(rangy.createClassApplier(
-      this.className,
-      {
-        ignoreWhiteSpace: true,
-        onElementCreate: (element)=>{temporaryElements.push(element)},
-        useExistingElements: false
-      }
-    ));
-
-    highlighter.highlightSelection(
-      this.className,
-      {exclusive: false}
-    );
-    if (temporaryElements.length > 0){
-      this.domElements = temporaryElements;
+    const range = this._selectRange(_startOffset, _endOffset);
+    const parts = [];
+    parts.push(range.start.node.textContent.substr(0, range.start.offset));
+    parts.push('<span class="' + this.className + '">');
+    if (range.start.node === range.end.node) {
+      parts.push(range.start.node.textContent.substr(range.start.offset, (range.end.offset - range.start.offset)));
+    } else {
+      parts.push(range.start.node.textContent.substr(range.start.offset));
+      parts.push('</span>');
+      parts.push('<span class="' + this.className + '">');
+      parts.push(range.end.node.textContent.substr(0, range.end.offset));
     }
-    selection.removeAllRanges();
+    parts.push('</span>');
+    parts.push(range.end.node.textContent.substr(range.end.offset));
+    if (range.start.node === range.end.node && range.start.node.parentNode.childNodes.length === 1) {
+      // current content is a single text content, update parent.innerHTML.
+      range.start.node.parentNode.innerHTML = parts.join('');
+    } else {
+      // current content is a part of HTML elements, update parent.childNodes list.
+    }
   }
 
   _selectRange(startBodyOffset, endBodyOffset) {
@@ -81,13 +102,7 @@ class Highlight {
 
     const start = this._nodeFromTextOffset(startBodyOffset);
     const end = this._nodeFromTextOffset(endBodyOffset);
-    const selection = rangy.getSelection();
-    const range = rangy.createRange();
-    range.setStart(start.node, start.offset);
-    range.setEnd(end.node, end.offset);
-    selection.setSingleRange(range);
-
-    return selection;
+    return {start: start, end: end};
   }
 
   _nodeFromTextOffset(offset, node){
@@ -99,7 +114,7 @@ class Highlight {
 
       if (child.nodeName == "#text"){
         if (offset <= child.textContent.length){
-          return {offset:offset, node:child};
+          return {offset: offset, node: child};
         }
         offset -= child.textContent.length;
       } else{
