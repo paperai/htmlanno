727bb629ae5c66275bf17862e85cf1afad483cfa
Kentarou Mamiya <kmamiya@logicalrabbit.jp>
2018-05-06 23:07:57 +0900

    #50 added comment, and refactoring - changed to the HTML parsing is executed only loadhtmlpromise,  because the annotation position becomes illegal (the cause is newline code removed by second parsing) - moved some function from Highlighter to HtmlView, because  to capsulize contents array in HtmlView class

--- src/htmlViewer.js
+++ src/htmlViewer.js
@@ -1,73 +1,105 @@
 class HtmlViewer {
     constructor() {
         this.contents = []
-        this.html_obj = undefined
+        this.body_node = undefined
     }
 
     /**
-     * @param doc Html source (String)
+     * (re)rendering HTML.
+     * @param doc HTMLElement of `<body>` tag (clear old content and rende) or undefined (re-render)
      */
     render(doc) {
         if (doc !== undefined) {
-            const parser = new DOMParser()
-            // Htmlanno uses the XHTML, but DOMParser accepted only 'text/html'.
-            this.html_obj = parser.parseFromString(doc, 'text/html').body
-
+            this.body_node = doc;
             this.contents = []
-            this.content_length = this._calculate_offset_and_register(this.html_obj, undefined, 0)
+            this.content_length = this._calculateOffsetAndRegister(this.body_node, undefined, 0)
         }
-        this.setDoc(this.html_obj.innerHTML)
-    }
-    _calculate_offset_and_register(html_node, parent_index, parent_offset) {
-        let last_offset = parent_offset
-        for(let index = 0; index < html_node.childNodes.length; index ++) {
-            const current_node = html_node.childNodes[index]
-            if (current_node.nodeName === '#text') {
-                last_offset += current_node.textContent.length
-            } else {
-                const current_index = this.contents.length
-                const id = this.contents.length + 1
-                current_node.setAttribute('data-htmlanno-id', id)
-                this.contents.push({ parent_index: parent_index, offset: last_offset, id: id })
-                last_offset = this._calculate_offset_and_register(current_node, current_index, last_offset)
-            }
-            // else; #comment, etc. ignore.
-        }
-        return last_offset;
+        const view = document.getElementById('viewer');
+        view.innerHTML = this.body_node.innerHTML;
     }
 
-    _findPositionIncluded(position, contents) {
+    /**
+     * search the index of contents that include position (First level, check only offset position each contents head)
+     * @param position offset from text content head
+     * @return index value of this.contents or -1 (not found)
+     */
+    findContentIndexThatIncludes(position) {
         let index;
-        for(index = 0; index < contents.length; index ++) {
-            if (contents[index].offset > position) {
-                // 1. 行き過ぎたので戻る
-                // 2. contents[index]に親はいるか？
-                //    2.1 親要素のテキストコンテンツにpositionが収まるまで順に親要素を探索
-                if (contents[index - 1].parent_index !== undefined) {
-                    return this._checkParentContentLength(position, contents, contents[index - 1].parent_index);
+        for(index = 0; index < this.contents.length; index ++) {
+            if (this.contents[index].offset > position) {
+                const siblingsContent = this.contents[index - 1];
+                // 1. I gone too far, need back.
+                // siblingsContent = |------- [position?] ----|
+                // contents[index] = |------------------------|
+                //                      ^ now
+                if (siblingsContent.parent_index !== undefined) {
+                    // 2. I am child, search on parent content (position exists on the parent or parent parent ...)
+                    // contents[parent_index] = <parent tag>|----- [position?] --|
+                    // siblingsContent        =             |--------------------|
+                    // contents[index]        =             |--------------------|
+                    // contents[index+n]      =             |--------------------|</parent tag>
+                    return this._checkContentLength(position, siblingsContent.parent_index);
                 } else {
-                    // 3. 親がいないなら1つ前の兄弟を見る (1つ前の兄弟の範囲にpositionがある筈)
-                    return (index - 1)
+                    // 3. I am top level content, position exists on the before content.
+                    return (index - 1);
                 }
             }
         }
         // TODO: おそらくはここの判定にバグがあり、最終行のアノテーションレンダリングが実行されていません
-        return contents[index -1].offset > position && this.content_length <= position ? (index - 1) : -1;
+        return siblingsContent.offset > position && siblingsContent.content_length <= position ? (index - 1) : -1;
     }
 
-    _checkParentContentLength(target_position, contents, parent_index) {
-        if (target_position > contents[parent_index].offset + parent_node.textContent.length) {
-            return this._checkParentContentLength(target_position, contents, contents[parent_index].parent_index);
-        } else {
-            return parent_index;
+    getContentsOffset(index) {
+        return this.contents[index].offset;
+    }
+
+    /**
+     * calculate offset from the text content head, and register to contents array.
+     * this method is called by recursive.
+     * @param html_node     target HTMLElement (when the first call; `<body>` tag, when recursive call; childNodes of html_node on calling base)
+     * @param parent_index  index of parent node (when the first call; undefined , when recursive call; integer)
+     * @param parent_offset offset from the text content head for html_node (when the first call; 0, when recursive call; offset at calling)
+     */
+    _calculateOffsetAndRegister(html_node, parent_index, parent_offset) {
+        let last_offset = parent_offset;
+        for(let index = 0; index < html_node.childNodes.length; index ++) {
+            const current_node = html_node.childNodes[index];
+            if (current_node.nodeName === '#text') {
+                last_offset += current_node.textContent.length;
+            } else {
+                const current_index = this.contents.length;
+                const id = this.contents.length + 1
+                current_node.setAttribute('data-htmlanno-id', id);
+                this.contents.push({
+                    parent_index: parent_index,
+                    offset: last_offset,
+                    id: id
+                });
+                last_offset = this._calculateOffsetAndRegister(current_node, current_index, last_offset);
+            }
         }
+        return last_offset;
     }
- 
 
-    // ViewerBase相当
-    setDoc(doc_html) {
-        const view = document.getElementById('viewer')
-        view.innerHTML = doc_html
+    /**
+     * search the index of contents that include position (Second level, check that position is included in offset + content-length)
+     * this method is called by recursive.
+     * @param position offset from text content head
+     * @param index    value of this.contents
+     * @return index value of this.contents
+     */
+    _checkContentLength(position, index) {
+        if (index === undefined) {
+            return -1;
+        }
+        const node = document.querySelector(`[data-htmlanno-id="${index + 1}"`);
+        if (position > this.contents[index].offset + node.textContent.length) {
+            // position exists on before or later to me, check my parent
+            return this._checkContentLength(position, this.contents[index].parent_index);
+        } else {
+            // found.
+            return index;
+        }
     }
 }
 
