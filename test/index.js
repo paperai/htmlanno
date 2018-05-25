cd40e6b8fad590af29bcfa16b4e78ac239c3812f
Kentarou Mamiya <kmamiya@logicalrabbit.jp>
2018-05-25 22:27:28 +0900

    added test for SpanAnnotation (copied from highlight_test)

--- test/index.js
+++ test/index.js
@@ -7,3 +7,4 @@ const HighlighterTest = require('./highlighter_test.js');
 const HtmlannoTest = require('./htmlanno_test.js');
 const RelationAnnotationTest = require('./relationannotation_test.js');
 const TomlToolTest = require('./tomltool_test.js');
+const SpanAnnotationTest = require('./spanannotation_test.js');
