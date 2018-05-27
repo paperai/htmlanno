b508c3301382d3550847be2285fdfb69a2cf5636
Kentarou Mamiya <kmamiya@logicalrabbit.jp>
2018-05-27 17:32:33 +0900

    #58 recovered `npm run watch`

--- webpack.config.js
+++ webpack.config.js
@@ -8,4 +8,10 @@ module.exports = {
   externals: {
     "jquery": "jQuery"
   },
+  devServer: {
+    // [webpack-dev-server's BUG] cannot use '0.0.0.0', use '<real server IP address>' or update to webpack-dev-server v2.4.3
+    // see https://github.com/webpack/webpack-dev-server/issues/882
+    host: 'localhost', 
+    port: 8080
+  }
 };
